aa1a7acc0f17d6a2a9394222a46d0e276d7ef844
Ruth Iascas <ruth.iascas@gmail.com>
2023-04-18 19:29:16 -0300

    Se sube pantalla de productos

---
+++ Login/AltaClientes.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace Login
+{
+    partial class FrmAltaClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelAltaClientes = new System.Windows.Forms.Label();
+            this.lblNombreCliente = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblApellidoCliente = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblDNI = new System.Windows.Forms.Label();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.btnGuardarCliente = new System.Windows.Forms.Button();
+            this.btnCancelarAlta = new System.Windows.Forms.Button();
+            this.lblFechaNacimiento = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnListaClientes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelAltaClientes
+            // 
+            this.labelAltaClientes.AutoSize = true;
+            this.labelAltaClientes.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAltaClientes.Location = new System.Drawing.Point(340, 31);
+            this.labelAltaClientes.Name = "labelAltaClientes";
+            this.labelAltaClientes.Size = new System.Drawing.Size(114, 21);
+            this.labelAltaClientes.TabIndex = 0;
+            this.labelAltaClientes.Text = "Alta de clientes";
+            this.labelAltaClientes.UseMnemonic = false;
+            // 
+            // lblNombreCliente
+            // 
+            this.lblNombreCliente.AutoSize = true;
+            this.lblNombreCliente.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreCliente.Location = new System.Drawing.Point(92, 141);
+            this.lblNombreCliente.Name = "lblNombreCliente";
+            this.lblNombreCliente.Size = new System.Drawing.Size(71, 21);
+            this.lblNombreCliente.TabIndex = 1;
+            this.lblNombreCliente.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(169, 138);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(144, 28);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // lblApellidoCliente
+            // 
+            this.lblApellidoCliente.AutoSize = true;
+            this.lblApellidoCliente.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellidoCliente.Location = new System.Drawing.Point(92, 198);
+            this.lblApellidoCliente.Name = "lblApellidoCliente";
+            this.lblApellidoCliente.Size = new System.Drawing.Size(70, 21);
+            this.lblApellidoCliente.TabIndex = 3;
+            this.lblApellidoCliente.Text = "Apellido:";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellido.Location = new System.Drawing.Point(169, 191);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(144, 28);
+            this.txtApellido.TabIndex = 4;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(112, 326);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(51, 21);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(169, 319);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(144, 28);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // lblDNI
+            // 
+            this.lblDNI.AutoSize = true;
+            this.lblDNI.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDNI.Location = new System.Drawing.Point(474, 141);
+            this.lblDNI.Name = "lblDNI";
+            this.lblDNI.Size = new System.Drawing.Size(40, 21);
+            this.lblDNI.TabIndex = 7;
+            this.lblDNI.Text = "DNI:";
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDNI.Location = new System.Drawing.Point(529, 138);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(144, 28);
+            this.txtDNI.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Datos personales";
+            this.label1.UseMnemonic = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Datos de usuario";
+            this.label2.UseMnemonic = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(464, 319);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 28);
+            this.textBox1.TabIndex = 12;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(378, 323);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(80, 21);
+            this.lblPass.TabIndex = 11;
+            this.lblPass.Text = "Password:";
+            // 
+            // btnGuardarCliente
+            // 
+            this.btnGuardarCliente.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarCliente.Location = new System.Drawing.Point(656, 390);
+            this.btnGuardarCliente.Name = "btnGuardarCliente";
+            this.btnGuardarCliente.Size = new System.Drawing.Size(83, 31);
+            this.btnGuardarCliente.TabIndex = 13;
+            this.btnGuardarCliente.Text = "Guardar";
+            this.btnGuardarCliente.UseVisualStyleBackColor = false;
+            // 
+            // btnCancelarAlta
+            // 
+            this.btnCancelarAlta.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelarAlta.Location = new System.Drawing.Point(531, 390);
+            this.btnCancelarAlta.Name = "btnCancelarAlta";
+            this.btnCancelarAlta.Size = new System.Drawing.Size(102, 31);
+            this.btnCancelarAlta.TabIndex = 14;
+            this.btnCancelarAlta.Text = "Cancelar";
+            this.btnCancelarAlta.UseVisualStyleBackColor = false;
+            // 
+            // lblFechaNacimiento
+            // 
+            this.lblFechaNacimiento.AutoEllipsis = true;
+            this.lblFechaNacimiento.AutoSize = true;
+            this.lblFechaNacimiento.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaNacimiento.Location = new System.Drawing.Point(368, 194);
+            this.lblFechaNacimiento.Name = "lblFechaNacimiento";
+            this.lblFechaNacimiento.Size = new System.Drawing.Size(155, 21);
+            this.lblFechaNacimiento.TabIndex = 15;
+            this.lblFechaNacimiento.Text = "Fecha de nacimiento:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(529, 187);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 28);
+            this.textBox2.TabIndex = 16;
+            // 
+            // btnListaClientes
+            // 
+            this.btnListaClientes.AccessibleName = "";
+            this.btnListaClientes.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListaClientes.Location = new System.Drawing.Point(412, 390);
+            this.btnListaClientes.Name = "btnListaClientes";
+            this.btnListaClientes.Size = new System.Drawing.Size(102, 31);
+            this.btnListaClientes.TabIndex = 17;
+            this.btnListaClientes.Text = "Ver Clientes";
+            this.btnListaClientes.UseVisualStyleBackColor = false;
+            // 
+            // FrmAltaClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnListaClientes);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblFechaNacimiento);
+            this.Controls.Add(this.btnCancelarAlta);
+            this.Controls.Add(this.btnGuardarCliente);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.lblDNI);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblApellidoCliente);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombreCliente);
+            this.Controls.Add(this.labelAltaClientes);
+            this.Name = "FrmAltaClientes";
+            this.Text = "Alta de Clientes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelAltaClientes;
+        private System.Windows.Forms.Label lblNombreCliente;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblApellidoCliente;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblDNI;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Button btnGuardarCliente;
+        private System.Windows.Forms.Button btnCancelarAlta;
+        private System.Windows.Forms.Label lblFechaNacimiento;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnListaClientes;
+    }
+}
---
+++ Login/Productos.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace Login
+{
+    partial class Productos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTituloProductos = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btnGuardarProducto = new System.Windows.Forms.Button();
+            this.btnCancelarProducto = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColCantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColSubcategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColSeleccionar = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.txtBoxCantidad = new System.Windows.Forms.TextBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.btnModificar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTituloProductos
+            // 
+            this.lblTituloProductos.AutoSize = true;
+            this.lblTituloProductos.Location = new System.Drawing.Point(370, 47);
+            this.lblTituloProductos.Name = "lblTituloProductos";
+            this.lblTituloProductos.Size = new System.Drawing.Size(55, 13);
+            this.lblTituloProductos.TabIndex = 0;
+            this.lblTituloProductos.Text = "Productos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre producto:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Categoria:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(414, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Subcategoria:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Seleccione una categoria",
+            "Muebles",
+            "Cocina",
+            "Baño",
+            "Deco Hogar",
+            "Textil",
+            "Uso Personal",
+            "Escritorio",
+            "Organizadores",
+            "Niños",
+            "Marcas destacadas"});
+            this.comboBox1.Location = new System.Drawing.Point(548, 119);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Seleccione una categoria",
+            "Muebles",
+            "Cocina",
+            "Baño",
+            "Deco Hogar",
+            "Textil",
+            "Uso Personal",
+            "Escritorio",
+            "Organizadores",
+            "Niños",
+            "Marcas destacadas"});
+            this.comboBox2.Location = new System.Drawing.Point(548, 169);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(160, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // btnGuardarProducto
+            // 
+            this.btnGuardarProducto.Location = new System.Drawing.Point(661, 235);
+            this.btnGuardarProducto.Name = "btnGuardarProducto";
+            this.btnGuardarProducto.Size = new System.Drawing.Size(91, 42);
+            this.btnGuardarProducto.TabIndex = 9;
+            this.btnGuardarProducto.Text = "Guardar producto";
+            this.btnGuardarProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelarProducto
+            // 
+            this.btnCancelarProducto.Location = new System.Drawing.Point(548, 235);
+            this.btnCancelarProducto.Name = "btnCancelarProducto";
+            this.btnCancelarProducto.Size = new System.Drawing.Size(91, 42);
+            this.btnCancelarProducto.TabIndex = 10;
+            this.btnCancelarProducto.Text = "Cancelar";
+            this.btnCancelarProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(548, 484);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(91, 42);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.TabStop = false;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColNombre,
+            this.ColCantidad,
+            this.ColCategoria,
+            this.ColSubcategoria,
+            this.ColSeleccionar});
+            this.dataGridView1.Location = new System.Drawing.Point(129, 322);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(545, 125);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // ColNombre
+            // 
+            this.ColNombre.HeaderText = "Nombre";
+            this.ColNombre.Name = "ColNombre";
+            // 
+            // ColCantidad
+            // 
+            this.ColCantidad.HeaderText = "Cantidad";
+            this.ColCantidad.Name = "ColCantidad";
+            // 
+            // ColCategoria
+            // 
+            this.ColCategoria.HeaderText = "Categoria";
+            this.ColCategoria.Name = "ColCategoria";
+            // 
+            // ColSubcategoria
+            // 
+            this.ColSubcategoria.HeaderText = "Subcategoria";
+            this.ColSubcategoria.Name = "ColSubcategoria";
+            // 
+            // ColSeleccionar
+            // 
+            this.ColSeleccionar.HeaderText = "Seleccionar";
+            this.ColSeleccionar.Name = "ColSeleccionar";
+            this.ColSeleccionar.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ColSeleccionar.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // txtBoxCantidad
+            // 
+            this.txtBoxCantidad.Location = new System.Drawing.Point(200, 177);
+            this.txtBoxCantidad.Name = "txtBoxCantidad";
+            this.txtBoxCantidad.Size = new System.Drawing.Size(160, 20);
+            this.txtBoxCantidad.TabIndex = 14;
+            this.txtBoxCantidad.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(126, 180);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(52, 13);
+            this.lblCantidad.TabIndex = 13;
+            this.lblCantidad.Text = "Cantidad:";
+            this.lblCantidad.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(661, 484);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(91, 42);
+            this.btnModificar.TabIndex = 15;
+            this.btnModificar.TabStop = false;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // Productos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 578);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.txtBoxCantidad);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnCancelarProducto);
+            this.Controls.Add(this.btnGuardarProducto);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTituloProductos);
+            this.Name = "Productos";
+            this.Text = "Productos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTituloProductos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btnGuardarProducto;
+        private System.Windows.Forms.Button btnCancelarProducto;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColCantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColCategoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColSubcategoria;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColSeleccionar;
+        private System.Windows.Forms.TextBox txtBoxCantidad;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Button btnModificar;
+    }
+}
