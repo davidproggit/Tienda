afe0669cba7aa172c1cdd13a8b9c3413d99d4a11
danielisab8 <isabtru620@gmail.com>
2023-04-22 04:12:43 -0300

    Creacion de clases

---
+++ CapaDeNegocio/Empleado.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDeNegocio
+{
+    public abstract class Empleado
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        private string _apellido;
+
+        public string Apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+
+        private double _sueldo;
+
+        public double Sueldo
+        {
+            get { return _sueldo; }
+            set { _sueldo = value; }
+        }
+
+        public Empleado() { }
+    }
+}
