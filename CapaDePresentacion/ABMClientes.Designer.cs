ff62645289b4a916ce94a67985f34ff5c8dc47bf
Ruth Iascas <ruth.iascas@gmail.com>
2023-04-22 15:33:37 -0300

    Se suben pantallas para proveedor, Cliente , y generador de reportes

---
+++ CapaDePresentacion/ABMClientes.Designer.cs
@@ -0,0 +1,311 @@
+﻿
+namespace CapaDePresentacion
+{
+    partial class FormClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbldni = new System.Windows.Forms.Label();
+            this.txtNombreCliente = new System.Windows.Forms.TextBox();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.txtBoxDNI = new System.Windows.Forms.TextBox();
+            this.lblNombreCliente = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblContacto = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColNombreCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColApellidoCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColFechaNac = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColDNICliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColPasswordCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColSeleccionarCliente = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.btnBuscarCliente = new System.Windows.Forms.Button();
+            this.btnGuardarCliente = new System.Windows.Forms.Button();
+            this.btnEliminarCliente = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(45, 124);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(47, 13);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(337, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fecha de nacimiento:";
+            // 
+            // lbldni
+            // 
+            this.lbldni.AutoSize = true;
+            this.lbldni.Location = new System.Drawing.Point(343, 124);
+            this.lbldni.Name = "lbldni";
+            this.lbldni.Size = new System.Drawing.Size(29, 13);
+            this.lbldni.TabIndex = 3;
+            this.lbldni.Text = "DNI:";
+            // 
+            // txtNombreCliente
+            // 
+            this.txtNombreCliente.Location = new System.Drawing.Point(98, 75);
+            this.txtNombreCliente.Name = "txtNombreCliente";
+            this.txtNombreCliente.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreCliente.TabIndex = 4;
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(98, 121);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(100, 20);
+            this.txtBox.TabIndex = 5;
+            // 
+            // txtBoxDNI
+            // 
+            this.txtBoxDNI.Location = new System.Drawing.Point(378, 121);
+            this.txtBoxDNI.Name = "txtBoxDNI";
+            this.txtBoxDNI.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxDNI.TabIndex = 7;
+            // 
+            // lblNombreCliente
+            // 
+            this.lblNombreCliente.AutoSize = true;
+            this.lblNombreCliente.Location = new System.Drawing.Point(45, 78);
+            this.lblNombreCliente.Name = "lblNombreCliente";
+            this.lblNombreCliente.Size = new System.Drawing.Size(47, 13);
+            this.lblNombreCliente.TabIndex = 9;
+            this.lblNombreCliente.Text = "Nombre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(337, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Administración de clientes";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(449, 75);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // lblContacto
+            // 
+            this.lblContacto.AutoSize = true;
+            this.lblContacto.Location = new System.Drawing.Point(45, 165);
+            this.lblContacto.Name = "lblContacto";
+            this.lblContacto.Size = new System.Drawing.Size(87, 13);
+            this.lblContacto.TabIndex = 12;
+            this.lblContacto.Text = "Datos de usuario";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(37, 202);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 13;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Location = new System.Drawing.Point(78, 199);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(164, 20);
+            this.txtBoxEmail.TabIndex = 14;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(327, 202);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 15;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.Location = new System.Drawing.Point(389, 199);
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxPassword.TabIndex = 16;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColNombreCliente,
+            this.ColApellidoCliente,
+            this.ColFechaNac,
+            this.ColDNICliente,
+            this.ColEmail,
+            this.ColPasswordCliente,
+            this.ColSeleccionarCliente});
+            this.dataGridView1.Location = new System.Drawing.Point(31, 289);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(742, 73);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // ColNombreCliente
+            // 
+            this.ColNombreCliente.HeaderText = "Nombre";
+            this.ColNombreCliente.Name = "ColNombreCliente";
+            // 
+            // ColApellidoCliente
+            // 
+            this.ColApellidoCliente.HeaderText = "Apellido";
+            this.ColApellidoCliente.Name = "ColApellidoCliente";
+            // 
+            // ColFechaNac
+            // 
+            this.ColFechaNac.HeaderText = "Fecha de nacimiento";
+            this.ColFechaNac.Name = "ColFechaNac";
+            // 
+            // ColDNICliente
+            // 
+            this.ColDNICliente.HeaderText = "DNI";
+            this.ColDNICliente.Name = "ColDNICliente";
+            // 
+            // ColEmail
+            // 
+            this.ColEmail.HeaderText = "Email";
+            this.ColEmail.Name = "ColEmail";
+            // 
+            // ColPasswordCliente
+            // 
+            this.ColPasswordCliente.HeaderText = "Password";
+            this.ColPasswordCliente.Name = "ColPasswordCliente";
+            // 
+            // ColSeleccionarCliente
+            // 
+            this.ColSeleccionarCliente.HeaderText = "Seleccionar";
+            this.ColSeleccionarCliente.Name = "ColSeleccionarCliente";
+            // 
+            // btnBuscarCliente
+            // 
+            this.btnBuscarCliente.Location = new System.Drawing.Point(689, 242);
+            this.btnBuscarCliente.Name = "btnBuscarCliente";
+            this.btnBuscarCliente.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscarCliente.TabIndex = 18;
+            this.btnBuscarCliente.Text = "Buscar";
+            this.btnBuscarCliente.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardarCliente
+            // 
+            this.btnGuardarCliente.Location = new System.Drawing.Point(589, 242);
+            this.btnGuardarCliente.Name = "btnGuardarCliente";
+            this.btnGuardarCliente.Size = new System.Drawing.Size(70, 23);
+            this.btnGuardarCliente.TabIndex = 19;
+            this.btnGuardarCliente.Text = "Guardar";
+            this.btnGuardarCliente.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarCliente
+            // 
+            this.btnEliminarCliente.Location = new System.Drawing.Point(689, 403);
+            this.btnEliminarCliente.Name = "btnEliminarCliente";
+            this.btnEliminarCliente.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarCliente.TabIndex = 20;
+            this.btnEliminarCliente.Text = "Eliminar";
+            this.btnEliminarCliente.UseVisualStyleBackColor = true;
+            // 
+            // FormClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminarCliente);
+            this.Controls.Add(this.btnGuardarCliente);
+            this.Controls.Add(this.btnBuscarCliente);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtBoxPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblContacto);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblNombreCliente);
+            this.Controls.Add(this.txtBoxDNI);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.txtNombreCliente);
+            this.Controls.Add(this.lbldni);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblApellido);
+            this.Name = "FormClientes";
+            this.Text = "Administración de clientes";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbldni;
+        private System.Windows.Forms.TextBox txtNombreCliente;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.TextBox txtBoxDNI;
+        private System.Windows.Forms.Label lblNombreCliente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblContacto;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtBoxPassword;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColNombreCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColApellidoCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColFechaNac;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColDNICliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColEmail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColPasswordCliente;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColSeleccionarCliente;
+        private System.Windows.Forms.Button btnBuscarCliente;
+        private System.Windows.Forms.Button btnGuardarCliente;
+        private System.Windows.Forms.Button btnEliminarCliente;
+    }
+}
---
+++ CapaDePresentacion/AlertadeProducto.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace CapaDePresentacion
+{
+    partial class AlertadeProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGenerarAlerta = new System.Windows.Forms.Label();
+            this.lblcantidadproducto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblMen = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColAlerNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColAlerCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColAlerSubcategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColAlerStock = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColAlerSeleccionar = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.btnGuardarAlerta = new System.Windows.Forms.Button();
+            this.btnAlertCancelar = new System.Windows.Forms.Button();
+            this.btnAlerQuitarProducto = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGenerarAlerta
+            // 
+            this.lblGenerarAlerta.AutoSize = true;
+            this.lblGenerarAlerta.Location = new System.Drawing.Point(332, 38);
+            this.lblGenerarAlerta.Name = "lblGenerarAlerta";
+            this.lblGenerarAlerta.Size = new System.Drawing.Size(99, 13);
+            this.lblGenerarAlerta.TabIndex = 0;
+            this.lblGenerarAlerta.Text = "Alerta de productos";
+            // 
+            // lblcantidadproducto
+            // 
+            this.lblcantidadproducto.AutoSize = true;
+            this.lblcantidadproducto.Location = new System.Drawing.Point(44, 296);
+            this.lblcantidadproducto.Name = "lblcantidadproducto";
+            this.lblcantidadproducto.Size = new System.Drawing.Size(117, 13);
+            this.lblcantidadproducto.TabIndex = 1;
+            this.lblcantidadproducto.Text = "Cantidad de productos:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(472, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "En esta pantalla se configurara el alerta dependiendo la cantidad de productos qu" +
+    "e esten en stock";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(167, 293);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lblMen
+            // 
+            this.lblMen.AutoSize = true;
+            this.lblMen.Location = new System.Drawing.Point(111, 332);
+            this.lblMen.Name = "lblMen";
+            this.lblMen.Size = new System.Drawing.Size(50, 13);
+            this.lblMen.TabIndex = 4;
+            this.lblMen.Text = "Mensaje:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 329);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColAlerNombre,
+            this.ColAlerCategoria,
+            this.ColAlerSubcategoria,
+            this.ColAlerStock,
+            this.ColAlerSeleccionar});
+            this.dataGridView1.Location = new System.Drawing.Point(114, 149);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(541, 69);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // ColAlerNombre
+            // 
+            this.ColAlerNombre.HeaderText = "Nombre";
+            this.ColAlerNombre.Name = "ColAlerNombre";
+            // 
+            // ColAlerCategoria
+            // 
+            this.ColAlerCategoria.HeaderText = "Categoria";
+            this.ColAlerCategoria.Name = "ColAlerCategoria";
+            // 
+            // ColAlerSubcategoria
+            // 
+            this.ColAlerSubcategoria.HeaderText = "Subcategoria";
+            this.ColAlerSubcategoria.Name = "ColAlerSubcategoria";
+            // 
+            // ColAlerStock
+            // 
+            this.ColAlerStock.HeaderText = "Stock";
+            this.ColAlerStock.Name = "ColAlerStock";
+            // 
+            // ColAlerSeleccionar
+            // 
+            this.ColAlerSeleccionar.HeaderText = "Seleccionar";
+            this.ColAlerSeleccionar.Name = "ColAlerSeleccionar";
+            // 
+            // btnGuardarAlerta
+            // 
+            this.btnGuardarAlerta.Location = new System.Drawing.Point(677, 399);
+            this.btnGuardarAlerta.Name = "btnGuardarAlerta";
+            this.btnGuardarAlerta.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardarAlerta.TabIndex = 7;
+            this.btnGuardarAlerta.Text = "Guardar";
+            this.btnGuardarAlerta.UseVisualStyleBackColor = true;
+            // 
+            // btnAlertCancelar
+            // 
+            this.btnAlertCancelar.Location = new System.Drawing.Point(580, 399);
+            this.btnAlertCancelar.Name = "btnAlertCancelar";
+            this.btnAlertCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlertCancelar.TabIndex = 8;
+            this.btnAlertCancelar.Text = "Cancelar";
+            this.btnAlertCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAlerQuitarProducto
+            // 
+            this.btnAlerQuitarProducto.Location = new System.Drawing.Point(441, 399);
+            this.btnAlerQuitarProducto.Name = "btnAlerQuitarProducto";
+            this.btnAlerQuitarProducto.Size = new System.Drawing.Size(116, 23);
+            this.btnAlerQuitarProducto.TabIndex = 9;
+            this.btnAlerQuitarProducto.Text = "Quitar producto";
+            this.btnAlerQuitarProducto.UseVisualStyleBackColor = true;
+            // 
+            // AlertadeProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAlerQuitarProducto);
+            this.Controls.Add(this.btnAlertCancelar);
+            this.Controls.Add(this.btnGuardarAlerta);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblMen);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblcantidadproducto);
+            this.Controls.Add(this.lblGenerarAlerta);
+            this.Name = "AlertadeProducto";
+            this.Text = "Alerta de Producto";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGenerarAlerta;
+        private System.Windows.Forms.Label lblcantidadproducto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblMen;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColAlerNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColAlerCategoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColAlerSubcategoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColAlerStock;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColAlerSeleccionar;
+        private System.Windows.Forms.Button btnGuardarAlerta;
+        private System.Windows.Forms.Button btnAlertCancelar;
+        private System.Windows.Forms.Button btnAlerQuitarProducto;
+    }
+}
