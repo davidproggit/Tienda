1b1a7d1571d0b089fe5b9b87500586a2460a0c3a
David Cardozo <davidcardozoemma@gmail.com>
2023-05-11 15:41:15 -0300

    Agregado: capa de entidades con clase vendedor

---
+++ CapaDeEntidades/Vendedor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDeEntidades
+{
+    public class Vendedor
+    {
+        private int _id;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        private string _apellido;
+
+        public string Apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+
+    }
+}
