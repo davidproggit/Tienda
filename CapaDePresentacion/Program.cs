99d789bd8c783df5748e0c878019c4ea2cf2ea8f
David Cardozo <davidcardozoemma@gmail.com>
2023-05-22 14:50:41 -0300

    Cambio de pantalla de inicio

--- CapaDePresentacion/Program.cs
+++ CapaDePresentacion/Program.cs
@@ -18,7 +18,7 @@ namespace CapaDePresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new InterfazProducto());
+            Application.Run(new Login());
         }
     }
 }
