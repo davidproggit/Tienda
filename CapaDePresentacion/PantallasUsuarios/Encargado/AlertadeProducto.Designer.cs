7460b52952db4d6227d7b173018d27a467304554
David Cardozo <davidcardozoemma@gmail.com>
2023-05-22 21:28:59 -0300

    Cambiado: diseño de la pantalla

--- CapaDePresentacion/PantallasUsuarios/Encargado/AlertadeProducto.Designer.cs
+++ CapaDePresentacion/PantallasUsuarios/Encargado/AlertadeProducto.Designer.cs
@@ -29,6 +29,8 @@ namespace CapaDePresentacion
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.lblGenerarAlerta = new System.Windows.Forms.Label();
             this.lblcantidadproducto = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -46,72 +48,96 @@ namespace CapaDePresentacion
             // lblGenerarAlerta
             // 
             this.lblGenerarAlerta.AutoSize = true;
+            this.lblGenerarAlerta.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblGenerarAlerta.Location = new System.Drawing.Point(498, 58);
             this.lblGenerarAlerta.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblGenerarAlerta.Name = "lblGenerarAlerta";
-            this.lblGenerarAlerta.Size = new System.Drawing.Size(148, 20);
+            this.lblGenerarAlerta.Size = new System.Drawing.Size(200, 26);
             this.lblGenerarAlerta.TabIndex = 0;
             this.lblGenerarAlerta.Text = "Alerta de productos";
             // 
             // lblcantidadproducto
             // 
             this.lblcantidadproducto.AutoSize = true;
-            this.lblcantidadproducto.Location = new System.Drawing.Point(66, 455);
+            this.lblcantidadproducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcantidadproducto.Location = new System.Drawing.Point(66, 511);
             this.lblcantidadproducto.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblcantidadproducto.Name = "lblcantidadproducto";
-            this.lblcantidadproducto.Size = new System.Drawing.Size(174, 20);
+            this.lblcantidadproducto.Size = new System.Drawing.Size(236, 26);
             this.lblcantidadproducto.TabIndex = 1;
             this.lblcantidadproducto.Text = "Cantidad de productos:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(112, 137);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(703, 20);
+            this.label1.Size = new System.Drawing.Size(955, 26);
             this.label1.TabIndex = 2;
             this.label1.Text = "En esta pantalla se configurara el alerta dependiendo la cantidad de productos qu" +
     "e esten en stock";
             // 
             // txtCantidadMinima
             // 
-            this.txtCantidadMinima.Location = new System.Drawing.Point(250, 451);
+            this.txtCantidadMinima.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidadMinima.Location = new System.Drawing.Point(331, 511);
             this.txtCantidadMinima.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCantidadMinima.Name = "txtCantidadMinima";
-            this.txtCantidadMinima.Size = new System.Drawing.Size(212, 26);
+            this.txtCantidadMinima.Size = new System.Drawing.Size(212, 32);
             this.txtCantidadMinima.TabIndex = 3;
             // 
             // lblMen
             // 
             this.lblMen.AutoSize = true;
-            this.lblMen.Location = new System.Drawing.Point(166, 511);
+            this.lblMen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMen.Location = new System.Drawing.Point(605, 511);
             this.lblMen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMen.Name = "lblMen";
-            this.lblMen.Size = new System.Drawing.Size(73, 20);
+            this.lblMen.Size = new System.Drawing.Size(100, 26);
             this.lblMen.TabIndex = 4;
             this.lblMen.Text = "Mensaje:";
             // 
             // txtMensaje
             // 
-            this.txtMensaje.Location = new System.Drawing.Point(250, 506);
+            this.txtMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensaje.Location = new System.Drawing.Point(770, 513);
             this.txtMensaje.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtMensaje.Name = "txtMensaje";
-            this.txtMensaje.Size = new System.Drawing.Size(212, 26);
+            this.txtMensaje.Size = new System.Drawing.Size(212, 32);
             this.txtMensaje.TabIndex = 5;
             // 
             // grilla
             // 
             this.grilla.AllowUserToAddRows = false;
+            this.grilla.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grilla.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.grilla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.grilla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ColID,
             this.ColNombre});
-            this.grilla.Location = new System.Drawing.Point(194, 237);
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.grilla.DefaultCellStyle = dataGridViewCellStyle2;
+            this.grilla.Location = new System.Drawing.Point(353, 209);
             this.grilla.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.grilla.Name = "grilla";
+            this.grilla.ReadOnly = true;
             this.grilla.RowHeadersWidth = 62;
-            this.grilla.Size = new System.Drawing.Size(383, 161);
+            this.grilla.Size = new System.Drawing.Size(516, 256);
             this.grilla.TabIndex = 6;
             // 
             // ColID
@@ -120,21 +146,24 @@ namespace CapaDePresentacion
             this.ColID.HeaderText = "ID";
             this.ColID.MinimumWidth = 8;
             this.ColID.Name = "ColID";
-            this.ColID.Width = 62;
+            this.ColID.ReadOnly = true;
+            this.ColID.Width = 72;
             // 
             // ColNombre
             // 
             this.ColNombre.HeaderText = "Nombre";
             this.ColNombre.MinimumWidth = 8;
             this.ColNombre.Name = "ColNombre";
+            this.ColNombre.ReadOnly = true;
             this.ColNombre.Width = 150;
             // 
             // btnGuardarAlerta
             // 
-            this.btnGuardarAlerta.Location = new System.Drawing.Point(1016, 614);
+            this.btnGuardarAlerta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarAlerta.Location = new System.Drawing.Point(1021, 614);
             this.btnGuardarAlerta.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnGuardarAlerta.Name = "btnGuardarAlerta";
-            this.btnGuardarAlerta.Size = new System.Drawing.Size(112, 35);
+            this.btnGuardarAlerta.Size = new System.Drawing.Size(122, 51);
             this.btnGuardarAlerta.TabIndex = 7;
             this.btnGuardarAlerta.Text = "Guardar";
             this.btnGuardarAlerta.UseVisualStyleBackColor = true;
@@ -142,10 +171,11 @@ namespace CapaDePresentacion
             // 
             // btnAlertCancelar
             // 
-            this.btnAlertCancelar.Location = new System.Drawing.Point(870, 614);
+            this.btnAlertCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlertCancelar.Location = new System.Drawing.Point(849, 614);
             this.btnAlertCancelar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAlertCancelar.Name = "btnAlertCancelar";
-            this.btnAlertCancelar.Size = new System.Drawing.Size(112, 35);
+            this.btnAlertCancelar.Size = new System.Drawing.Size(133, 51);
             this.btnAlertCancelar.TabIndex = 8;
             this.btnAlertCancelar.Text = "Cancelar";
             this.btnAlertCancelar.UseVisualStyleBackColor = true;
