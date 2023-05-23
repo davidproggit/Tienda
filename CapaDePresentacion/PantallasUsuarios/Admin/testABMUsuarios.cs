a6fd1e0ec845376f66f113837e1f988b2fc7f541
David Cardozo <davidcardozoemma@gmail.com>
2023-05-23 02:48:28 -0300

    Verificar campos vacios

--- CapaDePresentacion/PantallasUsuarios/Admin/testABMUsuarios.cs
+++ CapaDePresentacion/PantallasUsuarios/Admin/testABMUsuarios.cs
@@ -44,7 +44,13 @@ namespace CapaDePresentacion
         {
             ModeloUsuario objetoCapaNegocio = new ModeloUsuario();
 
-             objetoCapaNegocio.InsertarNuevoUsuario(txtUsuario.Text, txtClave.Text, txtNombre.Text, txtApellido.Text, txtEmail.Text, comboCargo.Text, txtDNI.Text, txtCuil.Text);
+            if (!CamposVacios())
+            {
+                objetoCapaNegocio.InsertarNuevoUsuario(txtUsuario.Text, txtClave.Text, txtNombre.Text, txtApellido.Text, txtEmail.Text, comboCargo.Text, txtDNI.Text, txtCuil.Text);
+                MessageBox.Show("Datos insertados");
+            }
+            else
+                MessageBox.Show("Error: tiene que completar todos los campos");
         }
 
         private void CargarRoles()
@@ -54,6 +60,14 @@ namespace CapaDePresentacion
             comboCargo.Items.Add("vendedor");
             comboCargo.Items.Add("encargado");
             comboCargo.Items.Add("gerente");
+        }
+
+        private bool CamposVacios()
+        {
+            return string.IsNullOrEmpty(txtUsuario.Text) || string.IsNullOrEmpty(txtClave.Text)
+                 || string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtApellido.Text)
+                 || string.IsNullOrEmpty(txtEmail.Text) || string.IsNullOrEmpty(comboCargo.Text)
+                 || string.IsNullOrEmpty(txtDNI.Text) || string.IsNullOrEmpty(txtCuil.Text);
         }
 
     }
