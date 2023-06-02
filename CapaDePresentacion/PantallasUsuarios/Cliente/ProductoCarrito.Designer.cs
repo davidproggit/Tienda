dd21b1097614681ce3036acda7c0d42e83fe3a7a
David Cardozo <davidcardozoemma@gmail.com>
2023-06-02 05:49:22 -0300

    Agregado: componente de producto en carrito

---
+++ CapaDePresentacion/PantallasUsuarios/Cliente/ProductoCarrito.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CapaDePresentacion.PantallasUsuarios.Cliente
+{
+    partial class ProductoCarrito
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(23, 81);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(104, 36);
+            this.lblPrecio.TabIndex = 5;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.Color.Transparent;
+            this.lblNombre.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.Color.Black;
+            this.lblNombre.Location = new System.Drawing.Point(24, 28);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(127, 36);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(24, 129);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(140, 36);
+            this.lblCantidad.TabIndex = 6;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // ProductoCarrito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "ProductoCarrito";
+            this.Size = new System.Drawing.Size(905, 220);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblCantidad;
+    }
+}
---
+++ CapaDePresentacion/PantallasUsuarios/Cliente/ProductoCarrito.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaDePresentacion.PantallasUsuarios.Cliente
+{
+    public partial class ProductoCarrito : UserControl
+    {
+        public ProductoCarrito()
+        {
+            InitializeComponent();
+        }
+
+        private int _id;
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _nombre;
+
+        public string nombre
+        {
+            get { return lblNombre.Text; }
+            set { lblNombre.Text = value; }
+        }
+
+        private string _descripcion;
+
+        public string descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+        private string _marca;
+
+        public string marca
+        {
+            get { return _marca; }
+            set { _marca = value; }
+        }
+
+        private string _precio;
+
+        public string precio
+        {
+            get { return lblPrecio.Text; }
+            set { lblPrecio.Text = value; }
+        }
+
+        private int _cantidad;
+
+        public int cantidad
+        {
+            get { return _cantidad; }
+            set { _cantidad = value; }
+        }
+      
+    }
+}
