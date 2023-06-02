21d0a79d227e1447bb9c5adf73180546f3d91e81
David Cardozo <davidcardozoemma@gmail.com>
2023-06-02 05:51:33 -0300

    Nueva pantalla de carrito

---
+++ CapaDePresentacion/PantallasUsuarios/Cliente/Carrito.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace CapaDePresentacion.PantallasUsuarios.Cliente
+{
+    partial class Carrito
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.contenedorProductos = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // contenedorProductos
+            // 
+            this.contenedorProductos.BackColor = System.Drawing.Color.Gray;
+            this.contenedorProductos.Location = new System.Drawing.Point(37, 72);
+            this.contenedorProductos.Name = "contenedorProductos";
+            this.contenedorProductos.Size = new System.Drawing.Size(795, 420);
+            this.contenedorProductos.TabIndex = 1;
+            // 
+            // Carrito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 558);
+            this.Controls.Add(this.contenedorProductos);
+            this.Name = "Carrito";
+            this.Text = "Carrito";
+            this.Load += new System.EventHandler(this.Carrito_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel contenedorProductos;
+    }
+}
---
+++ CapaDePresentacion/PantallasUsuarios/Cliente/Carrito.cs
@@ -0,0 +1,52 @@
+﻿using CapaComun;
+using CapaDeNegocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace CapaDePresentacion.PantallasUsuarios.Cliente
+{
+    public partial class Carrito : Form
+    {
+        private List<FormatoProductos> _valores = new List<FormatoProductos>();
+
+        public Carrito()
+        {
+            InitializeComponent();
+        }
+
+        private void Carrito_Load(object sender, EventArgs e)
+        {
+            Llenar();
+        }
+
+        private void Llenar()
+        {
+            Productos productos = new Productos();
+            _valores = productos.RellenarCarrito();
+
+            foreach (FormatoProductos datos in _valores)
+            {
+                ProductoCarrito productoCarrito = new ProductoCarrito();
+
+                productoCarrito.id = datos.id;
+                productoCarrito.nombre = datos.nombre;
+                productoCarrito.descripcion = datos.descripcion;
+                productoCarrito.marca = datos.marca;
+                productoCarrito.precio = "$" + datos.precio;
+                productoCarrito.cantidad = datos.cantidad;
+                //productoCarrito.enCarrito = datos.enCarrito;
+
+                contenedorProductos.Controls.Add(productoCarrito);
+            }
+        }
+       
+    }
+}
