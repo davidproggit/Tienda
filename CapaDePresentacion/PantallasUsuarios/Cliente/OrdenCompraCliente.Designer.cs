3998bae47fd14f2d7c26c651aeded2b7a6d4a307
David Cardozo <davidcardozoemma@gmail.com>
2023-06-04 21:58:54 -0300

    Agregado: componente orden de compra de cliente

---
+++ CapaDePresentacion/PantallasUsuarios/Cliente/OrdenCompraCliente.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CapaDePresentacion.PantallasUsuarios.Cliente
+{
+    partial class OrdenCompraCliente
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombreProducto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNombreProducto
+            // 
+            this.lblNombreProducto.AutoSize = true;
+            this.lblNombreProducto.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreProducto.ForeColor = System.Drawing.Color.DimGray;
+            this.lblNombreProducto.Location = new System.Drawing.Point(17, 67);
+            this.lblNombreProducto.Name = "lblNombreProducto";
+            this.lblNombreProducto.Size = new System.Drawing.Size(127, 36);
+            this.lblNombreProducto.TabIndex = 18;
+            this.lblNombreProducto.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(17, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 36);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Producto:";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.ForeColor = System.Drawing.Color.DimGray;
+            this.lblCantidad.Location = new System.Drawing.Point(18, 165);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(140, 36);
+            this.lblCantidad.TabIndex = 16;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.ForeColor = System.Drawing.Color.DimGray;
+            this.lblPrecio.Location = new System.Drawing.Point(17, 117);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(104, 36);
+            this.lblPrecio.TabIndex = 15;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.Location = new System.Drawing.Point(336, 84);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(112, 36);
+            this.lblEstado.TabIndex = 19;
+            this.lblEstado.Text = "Estado";
+            // 
+            // OrdenCompraCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.lblNombreProducto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblPrecio);
+            this.Name = "OrdenCompraCliente";
+            this.Size = new System.Drawing.Size(560, 218);
+            this.Load += new System.EventHandler(this.OrdenCompraCliente_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombreProducto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblEstado;
+    }
+}
---
+++ CapaDePresentacion/PantallasUsuarios/Cliente/OrdenCompraCliente.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaDePresentacion.PantallasUsuarios.Cliente
+{
+    public partial class OrdenCompraCliente : UserControl
+    {
+        public OrdenCompraCliente()
+        {
+            InitializeComponent();
+        }
+        private void OrdenCompraCliente_Load(object sender, EventArgs e)
+        {
+            CargarInformacion();
+        }
+
+        #region "Atributos"
+        private int _id;
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _nombre;
+
+        public string nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        private string _descripcion;
+
+        public string descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+        private string _marca;
+
+        public string marca
+        {
+            get { return _marca; }
+            set { _marca = value; }
+        }
+
+        private float _precio;
+
+        public float precio
+        {
+            get { return _precio; }
+            set { _precio = value; }
+        }
+
+        private int _cantidad;
+
+        public int cantidad
+        {
+            get { return _cantidad; }
+            set { _cantidad = value; }
+        }
+
+        private string _estado;
+
+        public string estado
+        {
+            get { return _estado; }
+            set { _estado = value; }
+        }
+
+        #endregion
+
+        private void CargarInformacion()
+        {
+            lblNombreProducto.Text = nombre;
+            lblPrecio.Text = "Precio: $" + precio.ToString();
+            lblCantidad.Text = "Cantidad: " + cantidad.ToString();
+            lblEstado.Text = estado;
+        }
+    }
+}
