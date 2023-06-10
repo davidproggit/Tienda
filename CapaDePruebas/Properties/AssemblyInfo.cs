b3e1cd7710cba95a58e8bec5fe54bff9949c3afa
David Cardozo <davidcardozoemma@gmail.com>
2023-06-10 17:17:23 -0300

    Pruebas unitarias de franco agregadas a la capa de pruebas

---
+++ CapaDePruebas/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CapaDePruebas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CapaDePruebas")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02709b84-2060-4646-b5e3-cc606c205e30")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CapaDePruebas/UnitTestProveedor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data;
+using CapaDeEntidades;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTestProveedor
+    {
+        [TestMethod]
+        public void VerificarNuevoProductoAgregado()
+        {
+            Proveedor proveedorTest = new Proveedor();
+            DataTable resultados = new DataTable();
+
+            proveedorTest.InsertarProductoStock("MuebleTest","Madera algarrobo","La mejor","1000","10");
+            resultados = proveedorTest.FiltrarProductoStock("MuebleTest"); 
+
+            Assert.IsTrue((resultados.Rows.Count) > 0);
+        }
+
+        [TestMethod]
+        public void BuscarProductoNoAgregado()
+        {
+            Proveedor proveedorTest = new Proveedor();
+            DataTable resultados = new DataTable();
+
+            resultados = proveedorTest.FiltrarProductoStock("@@@@@@@@");
+
+            Assert.IsTrue((resultados.Rows.Count) == 0);
+
+        }
+    }
+}
