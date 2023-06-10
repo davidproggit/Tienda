8f4cf6554633cbe3772f5bb3a724689db3fc65c4
FrancoCisneros123 <128562613+FrancoCisneros123@users.noreply.github.com>
2023-06-10 15:53:14 -0300

    Proyecto Test agregado
    El proyecto test contiene los test unitarios de las clases "Carrito", "Vendedor", "Administrador" y "Proveedor"

---
+++ UnitTestVendedor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data;
+using CapaDeEntidades;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTestVendedor
+    {
+        [TestMethod]
+        public void VerificarNuevoClienteCreado()
+        {
+            Vendedor vendedorTest = new Vendedor();
+            DataTable resultado = new DataTable();
+
+            vendedorTest.InsertarNuevoCliente("usuarioTest","1","user","test","userTest@gmail.com","Cliente","4444","1112");
+            resultado = vendedorTest.FiltrarCliente("user");
+
+            Assert.IsTrue((resultado.Rows.Count) != 0);
+
+        }
+
+        [TestMethod]
+        public void VerificarClienteEditado()
+        {
+            Vendedor vendedorTest = new Vendedor();
+            Vendedor vendedorTest1 = new Vendedor();
+            DataTable resultados = new DataTable();
+            string nombreUsuarioEditado;
+
+            
+            vendedorTest.EditarPerfilCliente(6,"ClienteTest", "1","Carlos", "testapellido", "a@email.com","Cliente", "1", "22");
+            resultados = vendedorTest1.FiltrarCliente("Carlos");
+            nombreUsuarioEditado = resultados.Rows[0]["Usuario"].ToString();
+
+            Assert.AreEqual("ClienteTest",nombreUsuarioEditado);
+        }
+    }
+}
